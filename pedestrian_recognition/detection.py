51846b4a272ac98da62918a797983de0c5b24826
Simon_Plazar <simon.plazar@gmail.com>
2023-05-24 00:11:21 +0200

    Detection frame color
    model obarva anotacijo glede na kako blizu je pešec

--- pedestrian_recognition/detection.py
+++ pedestrian_recognition/detection.py
@@ -2,6 +2,8 @@ import os
 import cv2
 import numpy as np
 from ultralytics import YOLO
+from pytictoc import TicToc
+import math
 
 # Load YOLOv8 model
 model = YOLO("yolov8n.pt")
@@ -43,6 +45,8 @@ prev_bounding_boxes = np.array([])
 new_bounding_boxess = np.array([])
 frame_count = 0
 
+t = TicToc()
+t.tic()
 while True:
     # Read frame
     ret, frame = video.read()
@@ -64,12 +68,22 @@ while True:
 
     # Draw each box from prev_bounding_boxes
     for box in prev_bounding_boxes:
+        box_size = math.dist((box[0], box[1]), (box[2], box[3]))
         cv2.putText(frame, "Person " + str(round(box[4], 3)), (int(box[0]*width_factor), int(box[1]*height_factor) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.rectangle(frame, (int(box[0]*width_factor), int(box[1]*height_factor)), (int(box[2]*width_factor), int(box[3]*height_factor)), (0, 255, 0), 2)
+        if(box_size > 180):
+            cv2.rectangle(frame, (int(box[0]*width_factor), int(box[1]*height_factor)), (int(box[2]*width_factor), int(box[3]*height_factor)), (0, 0, 255), 2)
+        elif (box_size > 100):
+            cv2.rectangle(frame, (int(box[0]*width_factor), int(box[1]*height_factor)), (int(box[2]*width_factor), int(box[3]*height_factor)), (0, 165, 255), 2)
+        else:
+            cv2.rectangle(frame, (int(box[0]*width_factor), int(box[1]*height_factor)), (int(box[2]*width_factor), int(box[3]*height_factor)), (0, 255, 0), 2)
 
     # Write frame to output video
     output_video.write(frame)
     frame_count += 1
+    # cv2.imshow('Frame', frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+t.toc()
 
 # Release resources
 video.release()
