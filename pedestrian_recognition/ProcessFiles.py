f8ba4d0da269b584d96db28ffd187056e78bc09c
Simon_Plazar <simon.plazar@gmail.com>
2023-06-15 20:46:40 +0200

    Main Fix

--- pedestrian_recognition/ProcessFiles.py
+++ pedestrian_recognition/ProcessFiles.py
@@ -1,4 +1,4 @@
-import Detect_files
+import DetectOnFile
 import os
 
 video_folder = os.path.join("no_audio", "Testiranje")
@@ -7,5 +7,5 @@ for video_name in os.listdir(video_folder):
     video_path = os.path.join(video_folder, video_name)
     output_video_path = os.path.join("Output", "Testiranje", "output_" + video_name)
 
-    Detect_files.Detect(video_path, output_video_path)
+    DetectOnFile.Detect(video_path, output_video_path)
     print(f"Done with {video_name}..\n")
