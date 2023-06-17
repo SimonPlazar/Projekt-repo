b711759f5e052305ba76399a05cebfe2ccfa3ae3
Simon_Plazar <simon.plazar@gmail.com>
2023-06-17 16:33:29 +0200

    getopt za processfiles

--- pedestrian_recognition/ProcessFiles.py
+++ pedestrian_recognition/ProcessFiles.py
@@ -1,11 +1,35 @@
+import getopt
+import sys
+
 import DetectOnFile
 import os
 
-video_folder = os.path.join("no_audio", "Testiranje")
-for video_name in os.listdir(video_folder):
-    print(f"Working on {video_name}..\n")
-    video_path = os.path.join(video_folder, video_name)
-    output_video_path = os.path.join("Output", "Testiranje", "output_" + video_name)
+argv = sys.argv[1:]
 
-    DetectOnFile.Detect(video_path, output_video_path)
+# pass argument to the program if video input or camera input
+opts, args = getopt.getopt(argv, "hd:", ["input=", "output="])
+dir = None
+
+
+for opt, arg in opts:
+    if opt == '-h':
+        print('predvajalnik.py [-i <video_name>][-c]')
+        sys.exit()
+    elif opt == '-d':
+        dir = arg
+
+if dir is None:
+    print('Direktorij ni podan!')
+    sys.exit()
+
+input_folder = os.path.join("Data", dir)
+output_folder = os.path.join("Output", dir)
+
+for video_name in os.listdir(input_folder):
+    print(f"Working on {video_name}..\n")
+
+    input_video_path = os.path.join(input_folder, video_name)
+    output_video_path = os.path.join(output_folder, "output_" + video_name)
+
+    DetectOnFile.Detect(input_video_path, output_video_path)
     print(f"Done with {video_name}..\n")
